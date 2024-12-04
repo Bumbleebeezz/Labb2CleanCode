fa8dc2cad58df9e7f6447aed5812b58a3d91451f
Maria Edström <mariat.edstrom@gmail.com>
2024-12-04 11:19:36 +0100

    API, DbContext started

---
+++ Dataccess/FeedbackDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dataccess
+{
+    public class FeedbackDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public FeedbackDbContext() { }
+        public FeedbackDbContext(DbContextOptions<FeedbackDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+    }
+}
